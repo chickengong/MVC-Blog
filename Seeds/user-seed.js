98efa8d7076a668c85a8900faf7f95e4242562ae
Ellexandra Cox <108504537+chickengong@users.noreply.github.com>
2023-01-10 16:39:02 -0600

    Update user-seed.js

--- Seeds/user-seed.js
+++ Seeds/user-seed.js
@@ -2,30 +2,16 @@ const { User } = require('../models');
 
 const userData = [
   {
-    username: "Cassie",
-    email: "cassie@gmail.com",
+    username: "Rachel",
+    email: "rachel@gmail.com",
     password: "password1234"
   },
   {
-    username: "Josh",
-    email: "josh@gmail.com",
+    username: "Bill",
+    email: "bill@gmail.com",
     password: "password1234"
   },
-  {
-    username: "Steven",
-    email: "steven@gmail.com",
-    password: "password1234"
-  },
-  {
-    username: "Emily",
-    email: "emily@gmail.com",
-    password: "password1234"
-  },
-  {
-    username: "John",
-    email: "john@gmail.com",
-    password: "password1234"
-  }
+  
 ];
 
 const seedUsers = () => User.bulkCreate(userData);
